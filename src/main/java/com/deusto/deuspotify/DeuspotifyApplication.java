1f287e76018a282a72c6d220954c19689036c182
AsierLoinaz <asier.loinaz@opendeusto.es>
2025-03-25 15:38:24 +0100

    Creation of Maven structure

---
+++ src/main/java/com/deusto/deuspotify/DeuspotifyApplication.java
@@ -0,0 +1,13 @@
+package com.deusto.deuspotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeuspotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeuspotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deusto/deuspotify/DeuspotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusto.deuspotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeuspotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
