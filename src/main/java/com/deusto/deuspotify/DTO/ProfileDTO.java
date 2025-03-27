d649ec4a08035cd75e1c6f319533dd39d81dca34
Javier <javier.g@opendeusto.es>
2025-03-27 16:53:24 +0100

    Crear ProfileDTO

---
+++ src/main/java/com/deusto/deuspotify/DTO/ProfileDTO.java
@@ -0,0 +1,49 @@
+package com.deusto.deuspotify.DTO;
+
+import java.util.List;
+
+public class ProfileDTO {
+
+    private Long id;
+    private String username;
+    private String email;
+    private List<String> friendsList;
+    private List<SongDTO> favouriteSongs;
+    private List<PlaylistDTO> playlists;
+    private boolean isAdmin;
+
+    public ProfileDTO() {}
+
+    public ProfileDTO(Long id, String username, String email, List<String> friendsList, List<SongDTO> favouriteSongs, List<PlaylistDTO> playlists, boolean isAdmin) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.friendsList = friendsList;
+        this.favouriteSongs = favouriteSongs;
+        this.playlists = playlists;
+        this.isAdmin = isAdmin;
+    }
+
+    // Getters and setters
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public List<String> getFriendsList() { return friendsList; }
+    public void setFriendsList(List<String> friendsList) { this.friendsList = friendsList; }
+
+    public List<SongDTO> getFavouriteSongs() { return favouriteSongs; }
+    public void setFavouriteSongs(List<SongDTO> favouriteSongs) { this.favouriteSongs = favouriteSongs; }
+
+    public List<PlaylistDTO> getPlaylists() { return playlists; }
+    public void setPlaylists(List<PlaylistDTO> playlists) { this.playlists = playlists; }
+
+    public boolean isAdmin() { return isAdmin; }
+    public void setAdmin(boolean isAdmin) { this.isAdmin = isAdmin; }
+}
