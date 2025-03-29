666bb9e66771d3128f275013aa2aa0c03469164c
Javier <javier.g@opendeusto.es>
2025-03-29 21:05:41 +0100

    añadir funciones findByNombre para un futuro

--- src/main/java/com/deusto/deuspotify/repositories/SongRepository.java
+++ src/main/java/com/deusto/deuspotify/repositories/SongRepository.java
@@ -1,10 +1,12 @@
 package com.deusto.deuspotify.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.deusto.deuspotify.model.Song;
 
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long> { 
-
+    Optional<Song> findByNombre(String nombre);
 }
