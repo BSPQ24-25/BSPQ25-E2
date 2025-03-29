046fae5e5a948f006198d377d52e871f69d73078
javiergutii <113512555+javiergutii@users.noreply.github.com>
2025-03-29 21:22:49 +0100

    Revert "Jon lasa façade"

--- src/main/java/com/deusto/deuspotify/repositories/SongRepository.java
+++ src/main/java/com/deusto/deuspotify/repositories/SongRepository.java
@@ -1,12 +1,10 @@
 package com.deusto.deuspotify.repositories;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.deusto.deuspotify.model.Song;
 
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long> { 
-    Optional<Song> findByNombre(String nombre);
+
 }
