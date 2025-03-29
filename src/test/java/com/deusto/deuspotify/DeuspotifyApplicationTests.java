ab8f3ba9f0e42a34e8a88d8b83e6cb57d41e18a9
Adrian Roman <adrian.roman@opendeusto.es>
2025-03-29 19:50:04 +0100

    Test test

--- src/test/java/com/deusto/deuspotify/DeuspotifyApplicationTests.java
+++ src/test/java/com/deusto/deuspotify/DeuspotifyApplicationTests.java
@@ -3,11 +3,16 @@ package com.deusto.deuspotify;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.deusto.deuspotify.repositories.ProfileRepository;
+
 @SpringBootTest
 class DeuspotifyApplicationTests {
 
+	private final ProfileRepository profileRepository;
+
 	@Test
-	void contextLoads() {
+	void finProfileByUsername() {
+	
 	}
 
 }
