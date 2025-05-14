6800edb8e6ba3b4590f9052c8247bbf692477c12
mariaviv27 <maria.vivar@opendeusto.es>
2025-05-14 16:03:38 +0200

    a ver si ahora funciona

--- src/main/resources/deuspotify-db-setup.sql
+++ src/main/resources/deuspotify-db-setup.sql
@@ -2,10 +2,10 @@
 CREATE DATABASE IF NOT EXISTS deuspotify;
 
 -- Crear usuario para conexiones remotas
-CREATE USER IF NOT EXISTS 'actions'@'%' IDENTIFIED BY 'actions';
+CREATE USER IF NOT EXISTS 'deuspotify_admin'@'%' IDENTIFIED BY 'adminpassword';
 
 -- Conceder privilegios al usuario
-GRANT ALL PRIVILEGES ON deuspotify.* TO 'actions'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON deuspotify.* TO 'deuspotify_admin'@'%' WITH GRANT OPTION;
 
 -- Aplicar cambios
 FLUSH PRIVILEGES;
