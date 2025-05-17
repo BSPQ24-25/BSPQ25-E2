a7581b237b6abb588825dda52486c71adba0abfa
anearrate <a.arrate@opendeusto.es>
2025-05-17 20:08:29 +0200

    repositories doc

--- src/main/java/com/deusto/deuspotify/repositories/SongRepository.java
+++ src/main/java/com/deusto/deuspotify/repositories/SongRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.deusto.deuspotify.model.Song;
 
+/**
+ * @interface SongRepository
+ * @brief Repository interface for {@link Song} entities.
+ *
+ * Provides basic CRUD operations for songs using Spring Data JPA.
+ */
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long> { 
 
